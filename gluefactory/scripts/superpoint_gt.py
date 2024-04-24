0a1a7bb1460223cf118ca41d62350e14747b1a84
Eckhard Goedeke <eckhard.goedeke@gmail.com>
2024-04-24 20:31:59 +0200

    add superpoint gt script

--- gluefactory/scripts/superpoint_gt.py
+++ gluefactory/scripts/superpoint_gt.py
@@ -1,6 +1,6 @@
 """
 Run the homography adaptation for all images in a given folder
-to regress and aggregate line distance function maps.
+to generate ground truth heatmap using superpoint.
 """
 
 import os
@@ -10,13 +10,41 @@ import cv2
 import h5py
 import torch
 from tqdm import tqdm
-from pytlsd import lsd
-from afm_op import afm
+#from pytlsd import lsd
 from joblib import Parallel, delayed
+from matplotlib import pyplot as plt
+import multiprocessing
 
-from ..datasets.utils.homographies import sample_homography, warp_lines
-from ..datasets.utils.data_augmentation import random_contrast
+# import Base model from /home/egoedeke/OneDrive/ETH/3DVision/glue-factory/gluefactory/models/base_model.py
+from gluefactory.models.base_model import BaseModel
 
+# import Superpoint from gluefactory/models/extractors/superpoint_open.py
+from gluefactory.models.extractors.superpoint_open import SuperPoint
+
+from gluefactory.geometry.homography import sample_homography_corners
+from gluefactory.utils.image import numpy_image_to_torch
+from gluefactory.geometry.homography import warp_points
+
+
+conf = {
+    "patch_shape": [800, 800],
+    "difficulty": 0.8,
+    "translation": 1.0,
+    "n_angles": 10,
+    "max_angle": 60,
+    "min_convexity": 0.05,
+}
+
+sp_conf = {
+    "max_num_keypoints": None,
+    "nms_radius": 4,
+    "detection_threshold": 0.005,
+    "remove_borders": 4,
+    "descriptor_dim": 256,
+    "channels": [64, 64, 128, 128, 256],
+    "dense_outputs": None,
+    "weights": None,  # local path of pretrained weights
+}
 
 homography_params = {
     'translation': True,
@@ -32,6 +60,16 @@ homography_params = {
 }
 
 
+
+def sample_homography(img, conf: dict, size: list):
+    data = {}
+    H, _, coords, _ = sample_homography_corners(img.shape[:2][::-1], **conf)
+    data["image"] = cv2.warpPerspective(img, H, tuple(size))
+    data["H_"] = H.astype(np.float32)
+    data["coords"] = coords.astype(np.float32)
+    data["image_size"] = np.array(size, dtype=np.float32)
+    return data
+
 def ha_df(img, num=100, border_margin=3, min_counts=5):
     """ Perform homography adaptation to regress line distance function maps.
     Args:
@@ -44,134 +82,128 @@ def ha_df(img, num=100, border_margin=3, min_counts=5):
         and the angle to the closest line.
     """
     h, w = img.shape[:2]
-    size = (w, h)
-    df_maps, angles, closests, counts = [], [], [], []
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,
-                                       (border_margin * 2, border_margin * 2))
-    pix_loc = np.stack(np.meshgrid(np.arange(h), np.arange(w), indexing='ij'),
-                       axis=-1)
-    raster_lines = np.zeros_like(img)
-
-    # Loop through all the homographies
-    for i in range(num):
-        # Generate a random homography
-        if i == 0:
-            H = np.eye(3)
-        else:
-            H = sample_homography(img.shape, **homography_params)
-        H_inv = np.linalg.inv(H)
-        
-        # Warp the image
-        warped_img = cv2.warpPerspective(img, H, size,
-                                         borderMode=cv2.BORDER_REPLICATE)
-        
-        # Regress the DF on the warped image
-        warped_lines = lsd(warped_img)[:, [1, 0, 3, 2]].reshape(-1, 2, 2)
-        
-        # Warp the lines back
-        lines = warp_lines(warped_lines, H_inv)
-        
-        # Get the DF and angles
-        num_lines = len(lines)
-        cuda_lines = torch.from_numpy(lines[:, :, [1, 0]].astype(np.float32))
-        cuda_lines = cuda_lines.reshape(-1, 4)[None].cuda()
-        offset = afm(
-            cuda_lines,
-            torch.IntTensor([[0, num_lines, h, w]]).cuda(), h, w)[0]
-        offset = offset[0].permute(1, 2, 0).cpu().numpy()[:, :, [1, 0]]
-        closest = pix_loc + offset
-        df = np.linalg.norm(offset, axis=-1)
-        angle = np.mod(np.arctan2(
-            offset[:, :, 0], offset[:, :, 1]) + np.pi / 2, np.pi)
-        
-        df_maps.append(df)
-        angles.append(angle)
-        closests.append(closest)
-        
-        # Compute the valid pixels
-        count = cv2.warpPerspective(np.ones_like(img), H_inv, size,
-                                    flags=cv2.INTER_NEAREST)
-        count = cv2.erode(count, kernel)
-        counts.append(count)
-        raster_lines += (df < 1).astype(np.uint8) * count 
-        
-    # Compute the median of all DF maps, with counts as weights
-    df_maps, angles = np.stack(df_maps), np.stack(angles)
-    counts, closests = np.stack(counts), np.stack(closests)
     
-    # Median of the DF
-    df_maps[counts == 0] = np.nan
-    avg_df = np.nanmedian(df_maps, axis=0)
+    aggregated_heatmap = np.zeros((w, h, num), dtype=np.float32)
+    agg_hm_old = np.zeros((w, h), dtype=np.float32)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
+    model = SuperPoint(sp_conf).to(device)
+    model.eval().to(device)
+    
+    with torch.no_grad():
+        
+    
+        # generate keypoints for validation
+        pred = model({"image": numpy_image_to_torch(img)[None].to(device)})
+        
+        # iterate over num homographies
+        for i in range(num):
+    
+            # warp image
+            homography = sample_homography(img, conf, [w,h])
+            
+            # apply detector
+            image_warped = homography["image"]
+            pred = model({"image": numpy_image_to_torch(image_warped)[None].to(device)})
+            pred = {k: v[0].cpu().numpy() for k, v in pred.items()}
+            #warped_heatmap = pred["heatmap"]
+            keyp = pred["keypoints"]
+            scores = pred["keypoint_scores"]
+            
+            warpedback_kp = warp_points(keyp, homography['H_'], inverse=True)
+            warpedback_kp = np.floor(warpedback_kp).astype(int)
+            
+            scores_xy = np.zeros((w, h), dtype=np.float32)
 
-    # Median of the closest
-    closests[counts == 0] = np.nan
-    avg_closest = np.nanmedian(closests, axis=0)
+            for j in range(len(warpedback_kp)):
+                x, y = warpedback_kp[j][0] + 1, warpedback_kp[j][1] + 1
+                if x < w and y < h:
+                    aggregated_heatmap[y, x, i] = scores[j]
+            
+            
+            # for each row j in warpedback_kp add scores[j] to aggregated_heatmap
+            for j in range(len(warpedback_kp)):
+                x, y = warpedback_kp[j][0], warpedback_kp[j][1]
+                if x < w and y < h:
+                    agg_hm_old[y, x] += scores[j]
+    
+    mask = aggregated_heatmap > 0
+        
+    aggregated_heatmap_nan = aggregated_heatmap.copy()
+    aggregated_heatmap_nan[~mask] = np.nan
+    
+    aggregated_heatmap_non_nan = np.nan_to_num(aggregated_heatmap_nan, nan=0.0)
+    median_scores_non_zero = np.nanmedian(aggregated_heatmap_non_nan, axis=2)
 
-    # Median of the angle
-    circ_bound = (np.minimum(np.pi - angles, angles)
-                  * counts).sum(0) / counts.sum(0) < 0.3
-    angles[:, circ_bound] -= np.where(
-        angles[:, circ_bound] > np.pi /2,
-        np.ones_like(angles[:, circ_bound]) * np.pi,
-        np.zeros_like(angles[:, circ_bound]))
-    angles[counts == 0] = np.nan
-    avg_angle = np.mod(np.nanmedian(angles, axis=0), np.pi)
+    
+    #plt.figure(figsize=(20,20))
+    #plt.imshow(median_scores_non_zero, cmap='viridis', interpolation='nearest', alpha=0.5)
+    #plt.show()
 
-    # Generate the background mask and a saliency score
-    raster_lines = np.where(raster_lines > min_counts, np.ones_like(img),
-                            np.zeros_like(img))
-    raster_lines = cv2.dilate(raster_lines, np.ones((21, 21), dtype=np.uint8))
-    bg_mask = (1 - raster_lines).astype(float)
-
-    return avg_df, avg_angle, avg_closest[:, :, [1, 0]], bg_mask
+    
+    return median_scores_non_zero
 
 
 def process_image(img_path, randomize_contrast, num_H, output_folder):
-    img = cv2.imread(img_path, 0)
-    if randomize_contrast is not None:
-        img = randomize_contrast(img)
+    img = cv2.imread(img_path)
+    
+    new_size = (800, 800)
+    resize_img = cv2.resize(img, new_size)
+    # convert from BGR to grayscale
+    img = cv2.cvtColor(resize_img, cv2.COLOR_BGR2GRAY)
+
     
     # Run homography adaptation
-    df, angle, closest, bg_mask = ha_df(img, num=num_H)
+    superpoint_heatmap = ha_df(img, num=num_H)
 
     # Save the DF in a hdf5 file
     out_path = os.path.splitext(os.path.basename(img_path))[0]
     out_path = os.path.join(output_folder, out_path) + '.hdf5'
     with h5py.File(out_path, "w") as f:
-        f.create_dataset("df", data=df.flatten())
-        f.create_dataset("line_level", data=angle.flatten())
-        f.create_dataset("closest", data=closest.flatten())
-        f.create_dataset("bg_mask", data=bg_mask.flatten())
+        f.create_dataset("superpoint_heatmap", data=superpoint_heatmap)
+        
 
-
-def export_ha(images_list, output_folder, num_H=100,
-              rdm_contrast=False, n_jobs=1):
+def export_ha(images_list, output_folder, num_H, n_jobs):
     # Parse the data
     with open(images_list, 'r') as f:
         image_files = f.readlines()
     image_files = [path.strip('\n') for path in image_files]
     
-    # Random contrast object
-    randomize_contrast = random_contrast() if rdm_contrast else None
+    multiprocessing.set_start_method('spawn')
+    
     
     # Process each image in parallel
     Parallel(n_jobs=n_jobs, backend='multiprocessing')(delayed(process_image)(
-        img_path, randomize_contrast, num_H, output_folder)
-                                            for img_path in tqdm(image_files))
+        img_path, None, num_H, output_folder)
+        for img_path in tqdm(image_files, total=len(image_files)))
+    
+    print("Done.")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('images_list', type=str,
-                        help='Path to a txt file containing the image paths.')
-    parser.add_argument('output_folder', type=str, help='Output folder.')
-    parser.add_argument('--num_H', type=int, default=100,
-                        help='Number of homographies used during HA.')
-    parser.add_argument('--random_contrast', action='store_true',
-                        help='Add random contrast to the images (disabled by default).')
-    parser.add_argument('--n_jobs', type=int, default=1,
-                        help='Number of jobs to run in parallel.')
-    args = parser.parse_args()
+    
+    
+    
 
-    export_ha(args.images_list, args.output_folder, args.num_H,
-              args.random_contrast, args.n_jobs)
+
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('images_list', type=str, help='Path to a txt file containing the image paths.')
+    parser.add_argument('output_folder', type=str, help='Output folder.')
+    parser.add_argument('--num_H', type=int, default=100, help='Number of homographies used during HA.')
+    parser.add_argument('--random_contrast', action='store_true', help='Add random contrast to the images (disabled by default).')
+    parser.add_argument('--n_jobs', type=int, default=5, help='Number of jobs to run in parallel.')
+    args = parser.parse_args()
+    
+    print("IMAGE LIST: ", args.images_list)
+    print("OUTPUT FOLDER: ", args.output_folder)
+    print("NUM H: ", args.num_H)
+    print("N JOBS: ", args.n_jobs)
+    
+
+    export_ha(args.images_list, args.output_folder, args.num_H, args.n_jobs)
+    
+    
+    
+    
+    
