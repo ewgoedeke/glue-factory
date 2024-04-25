b5d36e46d5e303c5e2fa10e88cd0044baf0b15ad
Raphael <rkreft@ethz.ch>
2024-04-25 15:51:10 +0200

    clean up gt generation file a bit

--- gluefactory/scripts/superpoint_gt.py
+++ gluefactory/scripts/superpoint_gt.py
@@ -10,21 +10,14 @@ import cv2
 import h5py
 import torch
 from tqdm import tqdm
-#from pytlsd import lsd
 from joblib import Parallel, delayed
-from matplotlib import pyplot as plt
 import multiprocessing
 
-# import Base model from /home/egoedeke/OneDrive/ETH/3DVision/glue-factory/gluefactory/models/base_model.py
-from gluefactory.models.base_model import BaseModel
-
-# import Superpoint from gluefactory/models/extractors/superpoint_open.py
 from gluefactory.models.extractors.superpoint_open import SuperPoint
 
 from gluefactory.geometry.homography import sample_homography_corners
 from gluefactory.utils.image import numpy_image_to_torch
 from gluefactory.geometry.homography import warp_points
-
 
 conf = {
     "patch_shape": [800, 800],
@@ -60,7 +53,6 @@ homography_params = {
 }
 
 
-
 def sample_homography(img, conf: dict, size: list):
     data = {}
     H, _, coords, _ = sample_homography_corners(img.shape[:2][::-1], **conf)
@@ -69,6 +61,7 @@ def sample_homography(img, conf: dict, size: list):
     data["coords"] = coords.astype(np.float32)
     data["image_size"] = np.array(size, dtype=np.float32)
     return data
+
 
 def ha_df(img, num=100, border_margin=3, min_counts=5):
     """ Perform homography adaptation to regress line distance function maps.
@@ -82,78 +75,63 @@ def ha_df(img, num=100, border_margin=3, min_counts=5):
         and the angle to the closest line.
     """
     h, w = img.shape[:2]
-    
+
     aggregated_heatmap = np.zeros((w, h, num), dtype=np.float32)
     agg_hm_old = np.zeros((w, h), dtype=np.float32)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    
+
     model = SuperPoint(sp_conf).to(device)
     model.eval().to(device)
-    
+
     with torch.no_grad():
-        
-    
-        # generate keypoints for validation
-        pred = model({"image": numpy_image_to_torch(img)[None].to(device)})
-        
         # iterate over num homographies
         for i in range(num):
-    
+
             # warp image
-            homography = sample_homography(img, conf, [w,h])
-            
+            homography = sample_homography(img, conf, [w, h])
+
             # apply detector
             image_warped = homography["image"]
             pred = model({"image": numpy_image_to_torch(image_warped)[None].to(device)})
             pred = {k: v[0].cpu().numpy() for k, v in pred.items()}
             #warped_heatmap = pred["heatmap"]
-            keyp = pred["keypoints"]
+            keypoints = pred["keypoints"]
             scores = pred["keypoint_scores"]
-            
-            warpedback_kp = warp_points(keyp, homography['H_'], inverse=True)
-            warpedback_kp = np.floor(warpedback_kp).astype(int)
-            
+
+            warped_back_kp = warp_points(keypoints, homography['H_'], inverse=True)
+            warped_back_kp = np.floor(warped_back_kp).astype(int)
+
             scores_xy = np.zeros((w, h), dtype=np.float32)
 
-            for j in range(len(warpedback_kp)):
-                x, y = warpedback_kp[j][0] + 1, warpedback_kp[j][1] + 1
+            for j in range(len(warped_back_kp)):
+                x, y = warped_back_kp[j][0] + 1, warped_back_kp[j][1] + 1
                 if x < w and y < h:
                     aggregated_heatmap[y, x, i] = scores[j]
-            
-            
-            # for each row j in warpedback_kp add scores[j] to aggregated_heatmap
-            for j in range(len(warpedback_kp)):
-                x, y = warpedback_kp[j][0], warpedback_kp[j][1]
+
+            # for each row j in warped_back_kp add scores[j] to aggregated_heatmap
+            for j in range(len(warped_back_kp)):
+                x, y = warped_back_kp[j][0], warped_back_kp[j][1]
                 if x < w and y < h:
                     agg_hm_old[y, x] += scores[j]
-    
+
     mask = aggregated_heatmap > 0
-        
+
     aggregated_heatmap_nan = aggregated_heatmap.copy()
     aggregated_heatmap_nan[~mask] = np.nan
-    
+
     median_scores_non_zero = np.nanmedian(aggregated_heatmap_nan, axis=2)
-    
 
-
-    
-    #plt.figure(figsize=(20,20))
-    #plt.imshow(median_scores_non_zero, cmap='viridis', interpolation='nearest', alpha=0.5)
-    #plt.show()
-
-    
     return median_scores_non_zero
 
 
 def process_image(img_path, randomize_contrast, num_H, output_folder):
     img = cv2.imread(img_path)
-    
+
     new_size = (800, 800)
     resize_img = cv2.resize(img, new_size)
     # convert from BGR to grayscale
     img = cv2.cvtColor(resize_img, cv2.COLOR_BGR2GRAY)
 
-    
     # Run homography adaptation
     superpoint_heatmap = ha_df(img, num=num_H)
 
@@ -162,49 +140,36 @@ def process_image(img_path, randomize_contrast, num_H, output_folder):
     out_path = os.path.join(output_folder, out_path) + '.hdf5'
     with h5py.File(out_path, "w") as f:
         f.create_dataset("superpoint_heatmap", data=superpoint_heatmap)
-        
+
 
 def export_ha(images_list, output_folder, num_H, n_jobs):
     # Parse the data
     with open(images_list, 'r') as f:
         image_files = f.readlines()
     image_files = [path.strip('\n') for path in image_files]
-    
+
     multiprocessing.set_start_method('spawn')
-    
-    
+
     # Process each image in parallel
     Parallel(n_jobs=n_jobs, backend='multiprocessing')(delayed(process_image)(
         img_path, None, num_H, output_folder)
-        for img_path in tqdm(image_files, total=len(image_files)))
-    
-    print("Done.")
+                                                       for img_path in tqdm(image_files, total=len(image_files)))
 
 
 if __name__ == "__main__":
-    
-    
-    
-
-
-    
     parser = argparse.ArgumentParser()
     parser.add_argument('images_list', type=str, help='Path to a txt file containing the image paths.')
     parser.add_argument('output_folder', type=str, help='Output folder.')
     parser.add_argument('--num_H', type=int, default=100, help='Number of homographies used during HA.')
-    parser.add_argument('--random_contrast', action='store_true', help='Add random contrast to the images (disabled by default).')
+    parser.add_argument('--random_contrast', action='store_true',
+                        help='Add random contrast to the images (disabled by default).')
     parser.add_argument('--n_jobs', type=int, default=5, help='Number of jobs to run in parallel.')
     args = parser.parse_args()
-    
+
     print("IMAGE LIST: ", args.images_list)
     print("OUTPUT FOLDER: ", args.output_folder)
     print("NUM H: ", args.num_H)
     print("N JOBS: ", args.n_jobs)
-    
 
     export_ha(args.images_list, args.output_folder, args.num_H, args.n_jobs)
-    
-    
-    
-    
-    
+    print("Done !")
