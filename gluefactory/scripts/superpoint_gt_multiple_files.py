199f9a42b6c87f5d1ea1dbc691f5a2e28df54f1c
Eckhard Goedeke <eckhard.goedeke@gmail.com>
2024-04-26 13:00:54 +0200

    fix mddir to include parent

--- gluefactory/scripts/superpoint_gt_multiple_files.py
+++ gluefactory/scripts/superpoint_gt_multiple_files.py
@@ -155,8 +155,9 @@ def process_image(img_data, num_H, output_folder_path):
     assert len(img_data["name"]) == 1  # Currently expect batch size one!
     # store gt in same structure as images of minidepth
     complete_out_folder = (output_folder_path / str(img_data["name"][0])).parent
+    complete_out_folder.mkdir(parents=True, exist_ok=True)
     output_file_path = complete_out_folder / f"{Path(img_data['name'][0]).name.split('.')[0]}.hdf5"
-    complete_out_folder.mkdir(exist_ok=True)
+    
     # Save the DF in a hdf5 file
     with h5py.File(output_file_path, "w") as f:
         f.create_dataset("superpoint_heatmap", data=superpoint_heatmap)
