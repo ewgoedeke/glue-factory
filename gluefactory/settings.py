3c04cac8ee494bd61a7472bba7bca14ed78f968b
Raphael <rkreft@ethz.ch>
2024-05-19 11:25:38 +0200

    revert: settings.py now again has the local  settings not the cluster ones

--- gluefactory/settings.py
+++ gluefactory/settings.py
@@ -2,5 +2,5 @@ from pathlib import Path
 
 root = Path(__file__).parent.parent  # top-level directory
 DATA_PATH = root / "data"  # datasets and pretrained weights
-TRAINING_PATH = root / "outputs/training/"  # training checkpoints
-EVAL_PATH = root / "outputs/results/"  # evaluation results
+TRAINING_PATH = root / "data/outputs/training/"  # training checkpoints
+EVAL_PATH = root / "data/outputs/results/"  # evaluation results
